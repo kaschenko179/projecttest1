15840ac1045b6179f83281dbc3419a2cb4f6f863
kaschenko179 <151081993+kaschenko179@users.noreply.github.com>
2026-02-21 12:18:51 +0200

    do while added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static void main() { {
 
         Scanner scanner = new Scanner(System.in);
+        String repeat;
 
-        System.out.println("Enter value a:");
-        int a = scanner.nextInt();
+        do {
+            System.out.println("Enter value a:");
+            int a = scanner.nextInt();
 
-        System.out.println("Enter value b:");
-        int b = scanner.nextInt();
+            System.out.println("Enter value b:");
+            int b = scanner.nextInt();
 
-        if (a < -20 || a > 20 || b < -20 || b > 20) {
-            System.out.println("Error: values must be in range -20 to 20");
-            return;
-        }
+            if (a < -20 || a > 20 || b < -20 || b > 20) {
+                System.out.println("Error: values must be in range -20 to 20");
+            } else {
 
-        EquationSolver solver = new EquationSolver();
-        double result = solver.solveEquation(a, b);
+                EquationSolver solver = new EquationSolver();
+                double result = solver.solveEquation(a, b);
+                System.out.println("Result: " + result);
+            }
 
-        System.out.println("Result: " + result);
+            System.out.println("Would you like to repeat the algorithm? (yes/no)");
+            repeat = scanner.next();
+
+        } while (repeat.equalsIgnoreCase("yes"));
+
+        scanner.close();
     }
 }
